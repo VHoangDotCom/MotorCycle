08e3c5cbd3a4f7d62239ad8e8396e41798686f38
quyet nguyen van <7784738-quyetnguyen02@users.noreply.gitlab.com>
2021-11-09 00:29:58 +0700

    few

---
+++ MotorCycle/database/migrations/2021_11_08_165401_create_categories_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('categoryCode',200);
+            $table->string('title',200);
+            $table->text('content');
+            $table->string('status',200);
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
